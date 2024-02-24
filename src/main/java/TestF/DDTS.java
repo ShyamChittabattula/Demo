f0baf77c088732960100bf08fdd0ee70dfee9ba6
shyamchittabattula <shyam99890@gmail.com>
2024-02-24 11:01:46 +0530

    this is my framework

---
+++ src/main/java/TestF/DDTS.java
@@ -0,0 +1,102 @@
+package TestF;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DDTS {
+
+		public static void main(String[] args) throws IOException  {
+			
+
+		
+			
+		    WebDriverWait wait;
+		    HSSFWorkbook workbook;
+		    HSSFSheet sheet;
+		    HSSFCell cell;
+			//System.setProperty("webdriver.chrome.driver", "E:\\Automation\\chromedriver.exe");
+	WebDriverManager.chromedriver().setup();
+			WebDriver driver=new ChromeDriver();
+			
+		     
+		    wait = new WebDriverWait(driver,30);
+		    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		
+		
+		
+		 // Import excel sheet.
+		     File src=new File("src\\main\\java\\TestF\\Book1.xls");
+		      
+		     // Load the file.
+		     FileInputStream finput = new FileInputStream(src);
+		      
+		     // Load he workbook.
+		    workbook = new HSSFWorkbook(finput);
+		      
+		     // Load the sheet in which data is stored.
+		     sheet= workbook.getSheetAt(0);
+		      
+		     for(int i=1; i<=sheet.getLastRowNum(); i++)
+		     {
+		    	 driver.get("https://www.amazon.in");
+		 	    
+		 	    driver.manage().window().maximize();
+		         // Import data for Email.
+		         cell = sheet.getRow(i).getCell(0);
+		         cell.getStringCellValue();
+		     //    driver.findElement(By.xpath("//input[@placeholder=\"First Name\"]")).sendKeys(cell.getStringCellValue());
+		         driver.findElement(By.id("twotabsearchtextbox")).sendKeys(cell.getStringCellValue());
+		         driver.findElement(By.id("nav-search-submit-button")).click();
+		         // Write data in the excel.
+		       FileOutputStream foutput=new FileOutputStream(src);
+		         
+		        // Specify the message needs to be written.
+		        String message = "pass";
+		        String message1 = "not found";
+	
+		       // if(driver.findElement(By.xpath("//*[text()='Full Name* ']")).isDisplayed())
+		        if(driver.getCurrentUrl().contains(cell.getStringCellValue()))
+		         {
+		        // Create cell where data needs to be written.
+		        sheet.getRow(i).createCell(1).setCellValue(message);
+		         }
+		        else
+		        {
+			        sheet.getRow(i).createCell(1).setCellValue(message1);
+	
+		        }
+		        
+		        // Specify the file in which data needs to be written.
+		        FileOutputStream fileOutput = new FileOutputStream(src);
+		         
+		        // finally write content
+		        workbook.write(fileOutput);
+		         
+		         // close the file
+		        fileOutput.close();
+		
+		
+		
+		
+		     }
+		
+		
+		
+	
+		}
+
+}
